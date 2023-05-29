a3a21753c965d656ff99e5f1d7e2f7784fd789e1
saitejaVundavilli <98604483+saitejaVundavilli@users.noreply.github.com>
2023-05-29 20:40:45 +0530

    def fun(x):     m=0     c=0     for i in range(1,x*2):         if x%i==0:             c+=1     if c==2:         m+=1     return m
    a=int(input())
    l=[]
    for i in range(1,a+1):
        if a%i==0:
            l.append(i)
    v=[]        
    for i in l:
        v.append(fun(i))
    print(len(l)-sum(v))

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,25 +1,19 @@
-def is_prime(j):
-    count=0
-    for i in range(1,j+1):
-        if(j%i==0):
-            count+=1
-    if(count==2):
-        return True
-    else:
-        return False
+def fun(x):
+    m=0
+    c=0
+    for i in range(1,x*2):
+        if x%i==0:
+            c+=1
+    if c==2:
+        m+=1
+    return m    
+        
 a=int(input())
-list_a=[]
-prime=0
+l=[]
 for i in range(1,a+1):
-    if(a%i==0):
-        list_a.append(i)
-for j in list_a:
-    if(is_prime(j)==False):
-        prime+=1
-    else:
-        pass
-print(prime)    
-        
-    
-    
-        
+    if a%i==0:
+        l.append(i)
+v=[]        
+for i in l:
+    v.append(fun(i))
+print(len(l)-sum(v))    
