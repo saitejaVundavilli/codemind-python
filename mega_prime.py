a69ccf9a9a6d81b9d584071b3f341db64634fb70
saitejaVundavilli <98604483+saitejaVundavilli@users.noreply.github.com>
2023-05-29 18:29:17 +0530

    def prime(a):     c=0     for i in range(1,a*2):         if a%i==0:             c+=1     if c==2:         return True     else:         return False x=int(input()) y=prime(x) v=str(x) z=prime(int(v[0])) n=prime(int(v[1])) if y and z and n:     print('Mega Prime') else:     print('Not Mega Prime')

--- mega_prime.py
+++ mega_prime.py
@@ -1,19 +1,18 @@
-def is_prime(b):
-    prime=0
-    for j in range(1,b+1):
-        if(b%j==0):
-            prime+=1
-    return prime        
-a=int(input())
-count=0
-for i in range(1,a+1):
-    if(a%i==0):
-        count+=1
-while a:
-    b=a%10
-    a=a//10
-    su=is_prime(b)
-if(count==2 and su==2):
-    print("Mega Prime")
+def prime(a):
+    c=0
+    for i in range(1,a*2):
+        if a%i==0:
+            c+=1
+    if c==2:
+        return True
+    else:
+        return False
+x=int(input())
+y=prime(x)
+v=str(x)
+z=prime(int(v[0]))
+n=prime(int(v[1]))
+if y and z and n:
+    print('Mega Prime')
 else:
-    print("Not Mega Prime")
+    print('Not Mega Prime')
