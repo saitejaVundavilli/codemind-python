bd2f5a450c32664afc2f5ca49f423ee4549c9977
saitejaVundavilli <98604483+saitejaVundavilli@users.noreply.github.com>
2023-05-29 19:40:00 +0530

    a,b=map(int,input().split()) x,y=[],[] for i in range(1,a*2):     x.append(i*a) for j in range(1,b*2):     y.append(j*b) for a in x:     if a in y:         print(a)         break

--- LCM.py
+++ LCM.py
@@ -1,11 +1,12 @@
-def ele(x):
-    l1=[]
-    for i in range(1,100):
-        l1.append(x*i)
-    return l1        
 a,b=map(int,input().split())
-lis=set(ele(a))
-lis2=set(ele(b))
-uni=lis&lis2
-print(min(uni))
-
+x,y=[],[]
+for i in range(1,a*2):
+    x.append(i*a)
+for j in range(1,b*2):
+    y.append(j*b)
+for a in x:
+    if a in y:
+        print(a)
+        break
+        
+    
