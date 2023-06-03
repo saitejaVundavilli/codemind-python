f2fb1bbe10260c228050d75d0a29e3c19a5c0545
saitejaVundavilli <98604483+saitejaVundavilli@users.noreply.github.com>
2023-06-03 09:36:39 +0530

    def is_prime(a):     c=0     for i in range(1,a+1):         if a%i==0:             c+=1         else:             pass     if c==2:         return True     else:         return False
    b=int(input())
    st=str(b)
    num=int(st[::-1])
    if is_prime(b) and is_prime(num):
        print('circular prime')
    elif is_prime(b)==True and is_prime(num)==False:
        print('prime but not a circular prime')
    else:
        print('not prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,21 +1,21 @@
-def is_prime(b):
-    prime=0
-    for j in range(1,b+1):
-        if(b%j==0):
-            prime+=1
-    return prime        
-a=int(input())
-count=0
-for i in range(1,a+1):
-    if(a%i==0):
-        count+=1
-word=str(a)
-al=word[::-1]
-b=int(al)
-call=is_prime(b)
-if(count==2 and call==2):
-    print("circular prime")
-elif(count==2 and call!=2):
-    print("prime but not a circular prime")
+def is_prime(a):
+    c=0
+    for i in range(1,a+1):
+        if a%i==0:
+            c+=1
+        else:
+            pass
+    if c==2:
+        return True
+    else:
+        return False
+        
+b=int(input())
+st=str(b)
+num=int(st[::-1])
+if is_prime(b) and is_prime(num):
+    print('circular prime')
+elif is_prime(b)==True and is_prime(num)==False:
+    print('prime but not a circular prime')
 else:
-    print("not prime")
+    print('not prime')
