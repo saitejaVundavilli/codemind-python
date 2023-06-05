db853af57f3b21b4f335022cb81b5a984dfb6051
saitejaVundavilli <98604483+saitejaVundavilli@users.noreply.github.com>
2023-06-05 15:43:45 +0530

    def is_prime(a):     c=0     for i in range(1,a+1):         if a%i==0:             c+=1     if c==2:         return True     else:         return False i=int(input()) j=int(input()) for k in range(i,j):     if is_prime(k)==True:         print(k)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,14 +1,14 @@
-def is_prime(j):
-    count=0
-    for i in range(1,j+1):
-        if(j%i==0):
-            count+=1
-    if(count==2):
+def is_prime(a):
+    c=0
+    for i in range(1,a+1):
+        if a%i==0:
+            c+=1
+    if c==2:
         return True
     else:
         return False
-a=int(input())
-b=int(input())
-for j in range(a,b+1):
-    if(is_prime(j)==True):
-        print(j)
+i=int(input())
+j=int(input())
+for k in range(i,j):
+    if is_prime(k)==True:
+        print(k)
