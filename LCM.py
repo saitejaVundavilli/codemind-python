9ef238e0098b3773383a1709bc855a63df3c8089
saitejaVundavilli <98604483+saitejaVundavilli@users.noreply.github.com>
2023-06-06 13:55:36 +0530

    from math import lcm a,b=map(int,input().split()) print(lcm(a,b))

--- LCM.py
+++ LCM.py
@@ -1,12 +1,3 @@
+from math import lcm
 a,b=map(int,input().split())
-x,y=[],[]
-for i in range(1,a*2):
-    x.append(i*a)
-for j in range(1,b*2):
-    y.append(j*b)
-for a in x:
-    if a in y:
-        print(a)
-        break
-        
-    
+print(lcm(a,b))
